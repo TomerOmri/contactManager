46bdec1787af930d0447f65c9371007bb2c0c175
Tomer Omri <tomer.omri@sap.com>
2019-02-23 18:39:41 +0200

    Updates

---
+++ precache-manifest.9eff041d2a1f9bc6bec75e072f793fb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "d2dbeb31db264cc5703f",
+    "url": "/contactmanager/static/js/main.d2dbeb31.chunk.js"
+  },
+  {
+    "revision": "46114c975937dfcb925a",
+    "url": "/contactmanager/static/js/1.46114c97.chunk.js"
+  },
+  {
+    "revision": "d2dbeb31db264cc5703f",
+    "url": "/contactmanager/static/css/main.f2970884.chunk.css"
+  },
+  {
+    "revision": "46114c975937dfcb925a",
+    "url": "/contactmanager/static/css/1.b78df274.chunk.css"
+  },
+  {
+    "revision": "89e5d3b093ec930c850d01476b4f874c",
+    "url": "/contactmanager/index.html"
+  }
+];
